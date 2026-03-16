6403baf7283b7b62bcb16c08eca644c40fe22938
najwafaizah <najwafaizah@gmail.com>
2026-03-16 15:42:28 +0700

    first commit

---
+++ src/hellowrold/pkg202557201033/Hellowrold202557201033.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hellowrold.pkg202557201033;
+
+/**
+ *
+ * @author X1 Carbon
+ */
+public class Hellowrold202557201033 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
